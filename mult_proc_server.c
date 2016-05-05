b25cc086f71702cb2d579f700046dc19fe0bd1db
Yongqi <985677283@qq.com>
2016-05-05 15:10:47 +0800

    Update mult_proc_server.c

--- mult_proc_server.c
+++ mult_proc_server.c
@@ -130,3 +130,73 @@ void error_handling(char *message)
     fputc('\n', stderr);
     exit(1);
 }
+
+//这个和上面的client.c二选一,这个是实现了读写分离,功能是一样的
+/*****************************************************
+    > File Name: echo_myclient.c
+    > Author: Yonqi
+    > Mail: haidaiheshi@126.com
+    > Created Time: 2016年05月02日 星期日 15时56分46秒
+注释1
+***************************************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <wait.h>
+#define BUF_SIZE 30
+
+void read_routine(int sock, char *buf);
+void write_routine(int sock, char *buf);
+int main(int argc, char *argv[])
+{
+    int sock;
+    pid_t pid;
+    char buf[BUF_SIZE];
+    struct sockaddr_in serv_adr;
+    if(argc!=3){
+        printf("usage: %s <ip> <port>\n", argv[0]);
+    }
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_adr.sin_port = htons(atoi(argv[2]));
+    if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1){
+        printf("connect error");
+    }
+    pid = fork();
+    if(pid==0){
+        write_routine(sock, buf);
+    }
+    else{
+        read_routine(sock, buf);
+    }
+    close(sock);
+    return 0;
+}
+void read_routine(int sock, char *buf)
+{
+    while(1){
+        int str_len = read(sock, buf, BUF_SIZE);
+        if(str_len==0){
+            return;
+        }
+        buf[str_len] = 0;
+        printf("message from serve is:%s\n", buf);
+    }
+}
+void write_routine(int sock, char *buf)
+{
+    while(1){
+        fgets(buf, BUF_SIZE, stdin);
+        if(!strcmp(buf, "q\n") || !strcmp(buf, "Q\n")){
+            shutdown(sock, SHUT_WR);
+            return;
+        }
+        write(sock, buf, strlen(buf));
+    }
+}
